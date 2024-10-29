b676fe3bd209057ed19e52b5075ef56e9fdeb31d
meirgoldshtein <m0548491057@gmail.com>
2024-10-29 18:05:45 +0200

    finish the weather components

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -8,45 +8,55 @@ export default function Weather() {
     const [error, setError] = useState<any>(false)
     const [errorMessage, setErrorMessage] = useState<string>('')
     const [loading, setLoading] = useState<boolean>(false)
-    useEffect(() => {
-        if (city) {
-            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d7f64e05043d19af9eb95658ec1a2f74`
-            const fetchData = async () => {
-                const response = await fetch(url)
-                const data = await response.json()
-                console.log(data)
-                setWeather(data)
-                data.cod == 200 ? setLoading(true) : setLoading(false)
-                data.cod == 200 ? setError(false)  : setError(true)
-                console.log(data.code)
-                data.message ? setErrorMessage(data.message) : setErrorMessage('')
-            }
-            
-            fetchData()
-            setCity('')
-            
+    const[degrees, setDegrees] = useState<'imperial' | 'metric'>('metric')
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${degrees}&appid=d7f64e05043d19af9eb95658ec1a2f74`
+    const fetchData = async () => {
+        try {
+            const response = await fetch(url)
+            const data = await response.json()
+            setWeather(data)
+            data.cod == 200 ? setLoading(true) : setLoading(false)
+            data.cod == 200 ? setError(false) : setError(true)
+            data.message ? setErrorMessage(data.message) : setErrorMessage('')
         }
-        else if (search) {
-            setSearch(false)
+        catch (err: any) {
             setError(true)
-            setErrorMessage('הכנס שם עיר')
+            setErrorMessage(err.message)
+            console.error(err)
         }
+    }
+
+    useEffect(() => {
+        const getData = async () => {
+            console.log(city)
+            if (city) {
+                await fetchData()
+            }
+            else if (search) {
+                setSearch(false)
+                setError(true)
+                setErrorMessage('הכנס שם עיר')
+            }
+        }
+
+        getData() // קריאה לפונקציה האסינכרונית
         return () => {
             console.log('יוצא מעמוד מזג אוויר')
         }
-        
-    }, [search])
-  return (
-    <div className='weather'>
-      <h1>Weather</h1>
-      <input type="text" placeholder='שם העיר' value={city} onChange={(e) => setCity(e.target.value) } />
-      <button onClick={() => setSearch(true)}>חיפוש</button>
-      <div className="weatherContent">
-        {loading && <WeatherDisplay weather={weather} />}
-        {error &&  errorMessage && <h1>{errorMessage}</h1>}
-        {error && !errorMessage && <h1>לא נמצאו תוצאות</h1>}
 
-      </div>
-    </div>
-  )
+    }, [search, degrees])
+    return (
+        <div className='weather'>
+            <h1>Weather</h1>
+            <input type="text" placeholder='שם העיר' value={city} onChange={(e) => setCity(e.target.value)} />
+            <button onClick={() => setSearch(!search)}>חיפוש</button>
+            <div className="weatherContent">
+                {loading && city && <WeatherDisplay weather={weather} setDegrees={setDegrees} degrees={degrees}/>}
+                {error && errorMessage && <h1>{errorMessage}</h1>}
+                {error && !errorMessage && <h1>לא נמצאו תוצאות</h1>}
+
+            </div>
+        </div>
+    )
 }
--- src/components/WeatherDisplay.tsx
+++ src/components/WeatherDisplay.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 interface Props {
     weather: any
+    setDegrees:(x:'imperial' | 'metric')=> void
+    degrees:'imperial' | 'metric'
 }
 
-export default function WeatherDisplay({weather}:Props) {
+export default function WeatherDisplay({weather, setDegrees, degrees}:Props) {
   return (
     <div className="weatherDisplay">
         <h1>Resault to {weather.name} Weather</h1>
+        <img src= {`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="icon" />
         <h2>Temp - {weather.main.temp}</h2>
-        <h2>Humidity - {weather.main.humidity}</h2>
+        <button onClick={() => setDegrees(degrees === 'metric' ? 'imperial' : 'metric')}>
+            {degrees === 'metric' ? 'celsius' : 'fahrenheit'}
+        </button>
     </div>
   )
 }
